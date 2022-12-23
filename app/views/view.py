ecf5d7d412b06292a75d8ee30c674177e9946858
David Simas <david.simas81@gmail.com>
2022-12-23 15:50:33 -0300

    Criandos as rotas na View

--- app/views/view.py
+++ app/views/view.py
@@ -4,7 +4,29 @@ from models.evento import Evento
 from models.usuario import Usuario
 
 
+# Renderização da página home.html
 @app.route("/")
 def index():
 
-    return render_template("index.html", titulo = "Agenda de Eventos")
+    return render_template("home.html", titulo = "Agenda de Eventos")
+
+
+# Renderização da página calendario.html
+@app.route("/calendario")
+def calendario():
+
+    return render_template("calendario.html", titulo = "Calendário de eventos")
+
+
+# Renderização da página sobre.html
+@app.route("/sobre")
+def sobre():
+
+    return render_template("sobre.html", titulo = "Desenvolvedores")
+
+
+# Renderização da página login.html
+@app.route("/login")
+def login():
+
+    return render_template("login.html", titulo = "Login")
